1cc0e94a0b80657d9c74eb5f533901a35160b561
adictz19 <56988688+adictz19@users.noreply.github.com>
2023-02-12 12:06:04 +0800

    Add files via upload

---
+++ config.js
@@ -0,0 +1,21 @@
+
+const CONFIG = {
+    titleWeb: "DATE TA SA FEB. 14",
+    introTitle: 'HELLO BEAUTIFUL LADY',
+    introDesc: `ARE YOU FREE THIS COMING FEBRUARY 14? HAHA
+    DO YOU WANNA GO OUT FOR A DATE?
+    
+    `,
+    btnIntro: 'YES OF COURSE',
+    title: 'CAN YOU BE MY VALENTINE? ON FEBRUARY 14, 2023',
+    desc: 'WILLING TO TAKE ME ON A DATE? ',
+    btnYes: 'YES <3',
+    btnNo: 'NO!',
+    question: 'YOU SURE YOU ARE SINGLE?',
+    btnReply: 'YES I AM SINGLE',
+    reply: 'Yêu thì yêu mà không yêu thì yêu <33333333',
+    mess: 'Book your appointment now! Hahaha',
+    messDesc: 'Proceed and chat me na!',
+    btnAccept: 'Chat Me! Haha',
+    messLink: 'https://www.facebook.com/geeerald.6991' 
+}
